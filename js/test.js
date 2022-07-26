f3d186be789adf3b1ec10ad8b700c2c548fda59b
cecilia <ceciliatangir@gmail.com>
2022-07-25 21:10:09 -0300

    Subida al repo

---
+++ js/test.js
@@ -0,0 +1,7 @@
+let adivinar = parseInt(prompt("Adiviná en qué número estoy pensando"));
+let x = 7;
+
+while (adivinar != x){
+    alert("Nop, no adivinaste.");
+    adivinar = prompt ("Probá de nuevo");
+    }
